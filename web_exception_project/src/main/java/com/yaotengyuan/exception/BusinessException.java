c873786182bdad4e968d16af2d8341336c14f503
yaotengyuan <yaotengyuan@thunisoft.com>
2017-08-21 12:23:50 +0800

    Revert "Revert "Revert "Revert "Revert "revert"""""
    This reverts commit b56d7119dfaf4f0a4932a9b9b1acd5dceebb7d52.

--- web_exception_project/src/main/java/com/yaotengyuan/exception/BusinessException.java
+++ web_exception_project/src/main/java/com/yaotengyuan/exception/BusinessException.java
@@ -1,11 +1,13 @@
 package com.yaotengyuan.exception;
 
 public class BusinessException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
-
-    public BusinessException(Object Obj) {
-        super(Obj.toString());
-    }
-
+	public BusinessException(Object Obj) {
+		super(Obj.toString());
+	}
+	public void Business() {
+	   System.out.println("111111");
+	}
 }
